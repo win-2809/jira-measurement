323c491a41bbf2433181fcc587e27f3c713cf018
Win Le <62741678+win-2809@users.noreply.github.com>
2021-09-16 08:08:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import tracker
 import datetime
 import plotly.graph_objs as go
 
-st.set_page_config(
-    page_title="Jira Tracker",
-    page_icon="✨",
-    initial_sidebar_state="collapsed"
-)
+# st.set_page_config(
+#     page_title="Jira Tracker",
+#     page_icon="✨",
+#     initial_sidebar_state="collapsed"
+# )
 
 st.title('Jira Tracker')
 
