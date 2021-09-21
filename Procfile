e5f20bb727e70607a57c484a3f799b9c524dc9cd
Win Le <tlv@localizedirect.com>
2021-09-21 22:06:59 +0700

    commit

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-web: sh setup.sh && pip install requirements.txt && streamlit run main.py
-=======
 web: sh setup.sh && streamlit run main.py
->>>>>>> cebafefb170843d3daca17976b6420cb65303f54
--- setup.sh
+++ setup.sh
@@ -6,8 +6,4 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-<<<<<<< HEAD
 " > ~/.streamlit/config.toml
-=======
-" > ~/.streamlit/config.toml
->>>>>>> cebafefb170843d3daca17976b6420cb65303f54
